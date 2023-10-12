c038150949f3bf095cd19264ce480b9c3de2453c
d3m0l15h <davicmax12@gmail.com>
2023-10-12 09:10:54 +0700

    Update Database

--- Models/GameOwned.cs
+++ Models/GameOwned.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace IS220_WebApplication.Models;
 
+[Keyless]
+[Table("game_owned")]
+[Index("GameId", Name = "FK_GameOwner_Game")]
+[Index("UserEmail", Name = "FK_GameOwner_User")]
 public partial class GameOwned
 {
+    [Column("userEmail")]
+    [StringLength(100)]
     public string UserEmail { get; set; } = null!;
 
+    [Column("gameID", TypeName = "int(10) unsigned")]
     public uint GameId { get; set; }
 
+    [ForeignKey("GameId")]
     public virtual Game Game { get; set; } = null!;
 
+    [ForeignKey("UserEmail")]
     public virtual User UserEmailNavigation { get; set; } = null!;
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,27 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace IS220_WebApplication.Models;
 
+[Table("user")]
+[Index("Username", Name = "username", IsUnique = true)]
 public partial class User
 {
+    [Column("username")]
+    [StringLength(50)]
     public string Username { get; set; } = null!;
 
+    [Column("password", TypeName = "text")]
     public string Password { get; set; } = null!;
 
+    [Key]
+    [Column("email")]
+    [StringLength(100)]
     public string Email { get; set; } = null!;
 
+    [Column("firstName")]
+    [StringLength(50)]
     public string? FirstName { get; set; }
 
+    [Column("lastName")]
+    [StringLength(50)]
     public string? LastName { get; set; }
 
+    [Column("phone")]
+    [StringLength(20)]
     public string? Phone { get; set; }
 
+    [Column("birth", TypeName = "date")]
     public DateTime? Birth { get; set; }
 
+    [Column("role", TypeName = "tinyint(4)")]
     public sbyte Role { get; set; }
 
+    [Column("created", TypeName = "datetime")]
     public DateTime Created { get; set; }
 
+    [Column("modified", TypeName = "datetime")]
     public DateTime? Modified { get; set; }
+
+    [Column("cash", TypeName = "int(11)")]
+    public int Cash { get; set; }
+
+    [InverseProperty("EmailNavigation")]
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
 }
