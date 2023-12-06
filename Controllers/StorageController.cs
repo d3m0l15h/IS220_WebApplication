e261da0e6de6f6ea81b3feb0eaf2f4d3ee279f78
d3m0l15h <davicmax12@gmail.com>
2023-12-06 13:47:15 +0700

    commit

--- Controllers/StorageController.cs
+++ Controllers/StorageController.cs
@@ -2,9 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IS220_WebApplication.Controllers;
 
-public class VietHoaGameController : Controller
+public class StorageController : Controller
 {
-    // GET
     public IActionResult Index()
     {
         return View();
