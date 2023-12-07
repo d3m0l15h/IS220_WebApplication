e3a88b133f3ff882f89459632abc0d39f82ee1c5
Cuong Phan <21520673@gm.uit.edu.vn>
2023-12-07 19:30:10 +0700

    Update routes and fetch data from db for game download pages

--- Controllers/DownloadController.cs
+++ Controllers/DownloadController.cs
@@ -1,12 +1,27 @@
+using IS220_WebApplication.Context;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IS220_WebApplication.Controllers;
-
-public class DownloadController : Controller
+namespace IS220_WebApplication.Controllers
 {
-    // GET
-    public IActionResult Index()
+    public class DownloadController : Controller
     {
-        return View();
+        private readonly MyDbContext _db;
+
+        public DownloadController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("download/{title}-{id}")]
+        public async Task<IActionResult> Index(string title, uint id)
+        {
+            var game = await _db.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Game = game;
+            return View();
+        }
     }
 }
