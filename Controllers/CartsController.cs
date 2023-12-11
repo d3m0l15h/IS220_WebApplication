2037be52b015ed70ba452e3ba6210008d24d3d50
Cuong Phan <21520673@gm.uit.edu.vn>
2023-12-12 00:33:14 +0700

    Fix formatting and add functionality to add game to cart

---
+++ Controllers/CartsController.cs
@@ -0,0 +1,46 @@
+using System.Security.Claims;
+using IS220_WebApplication.Context;
+using IS220_WebApplication.Models;
+using IS220_WebApplication.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreHero.ToastNotification.Abstractions;
+
+namespace IS220_WebApplication.Controllers;
+
+public class CartsController : Controller
+{
+    private readonly MyDbContext _db;
+
+    public CartsController(MyDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // [Route("cart/{title}-{id}")]
+    [HttpPost]
+    public IActionResult Add(uint game_id)
+    {
+        if (ModelState.IsValid)
+        {
+            string uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var a = new Cart()
+            {
+                Uid = uint.Parse(uid),
+                GameId = game_id
+            };
+            _db.Carts.Add(a);
+            _db.SaveChanges();
+            return Ok("Success");
+        }
+        else
+        {
+            return View();
+        }
+    }
+}
