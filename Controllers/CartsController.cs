c9c870757918e22ed0d2d912ee8bae6df57ec3f5
d3m0l15h <davicmax12@gmail.com>
2023-12-18 21:25:48 +0700

    search feature

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -25,58 +25,50 @@ public class CartsController : Controller
     [HttpPost]
     public IActionResult Add(uint game_id)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid) return RedirectToAction("index", "home");
+        var uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var userId = uint.Parse(uid);
+
+        // Check if the game_id already exists in the cart for this user
+        if (_db.Carts.Any(c => c.Uid == userId && c.GameId == game_id))
         {
-            string uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            uint userId = uint.Parse(uid);
-
-            // Check if the game_id already exists in the cart for this user
-            if (_db.Carts.Any(c => c.Uid == userId && c.GameId == game_id))
-            {
-                return Ok("The game is already in your cart.");
-            }
-
-            var cartItem = new Cart()
-            {
-                Uid = userId,
-                GameId = game_id
-            };
-            _db.Carts.Add(cartItem);
-            _db.SaveChanges();
-            return Ok("Successfully added to cart.");
+            return Ok("The game is already in your cart.");
         }
 
+        var cartItem = new Cart()
+        {
+            Uid = userId,
+            GameId = game_id
+        };
+        _db.Carts.Add(cartItem);
+        _db.SaveChanges();
+        return Ok("Successfully added to cart.");
+
         // return View();
-        return RedirectToAction("index", "home");
     }
     [HttpGet]
     public IActionResult Get()
     {
-        if (ModelState.IsValid)
-        {
-            string uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userCart = _db.Carts
-                .Where(c => c.Uid == uint.Parse(uid))
-                .Join(
-                    _db.Games,
-                    cart => cart.GameId,
-                    game => game.Id,
-                    (cart, game) => new CartDetail
-                    {
-                        GameId = game.Id,
-                        GameTitle = game.Title,
-                        GameImg = game.ImgPath,
-                        GamePrice = game.Price,
-                        Quantity = cart.Quantity
-                    }
-                )
-                .ToList();
-            return Ok(userCart);
-        }
-        else
-        {
-            return View();
-        }
+        if (!ModelState.IsValid) return RedirectToAction("index","home");
+        var uid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var userCart = _db.Carts
+            .Where(c => c.Uid == uint.Parse(uid))
+            .Join(
+                _db.Games,
+                cart => cart.GameId,
+                game => game.Id,
+                (cart, game) => new CartDetail
+                {
+                    GameId = game.Id,
+                    GameTitle = game.Title,
+                    GameImg = game.ImgPath,
+                    GamePrice = game.Price,
+                    Quantity = cart.Quantity
+                }
+            )
+            .ToList();
+        return Ok(userCart);
+
     }
     [HttpPost]
     public IActionResult Remove(uint game_id)
